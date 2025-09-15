bb021ce0a7c699a9d6a00fa0fa1adc396ce45946
Silvano Catinella <silvano.catinella@fimer.com>
2025-09-15 10:04:14 +0200

    (empty) integration tests added

---
+++ firmware/HILO_interface/main/iTest__wirelessAP.c
@@ -0,0 +1,48 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+// Filename: iTest__wirelessAP.c
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This target aims to implement a wireless access point
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <stdio.h>
+
+#ifndef HILO_MYSSIDNAME
+#define HILO_MYSSIDNAME "HILO9000"
+#endif
+
+#ifndef HILO_MYPASSWORD
+#define HILO_MYPASSWORD "HILOPWD"
+#endif
+
+void main() {
+
+
+	return;
+}
---
+++ firmware/HILO_interface/main/iTest__wirelessExternalNet.c
@@ -0,0 +1,55 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+//
+// Filename: iTest__wirelessExternalNet.c
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This integration test aims to check for the capability to connect itself to a remote wireless access point.
+//		In order to performs this test you must have a wireless access point, and the data of one available account
+//		in that device. In the CMake configuration file, you have to set the following symbols:
+//			HILO_SSIDNAME
+//			HILO_PASSWORD
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <stdio.h>
+
+#ifndef HILO_SSIDNAME
+#define HILO_SSIDNAME ""
+#endif
+
+#ifndef HILO_PASSWORD
+#define HILO_PASSWORD ""
+#endif
+
+void main() {
+
+
+	return;
+}
