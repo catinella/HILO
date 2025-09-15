0acec2b44716c47bfa47792290d778e99f45b2f5
Silvano Catinella <silvano.catinella@fimer.com>
2025-09-15 17:55:21 +0200

    New component for the Wi-Fi network management

---
+++ firmware/HILO_interface/components/wifiNetwork/wifiNetwork.c
@@ -0,0 +1,122 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+//
+// Filename: wifiNetwork.c
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This module contains all the functions used by the ESP32 to connect itself to the client using wireless
+//		network or to become itself a wireless access point
+//	
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <string.h>
+#include <wifiNetwork.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+
+wError wifi_connToExtNet (const char *ssidStr, const char *pwdStr, esp_event_handler_t evh) {
+	//
+	// Description:
+	//	This function allows you to connect the ESP32 device to the argument defined wireless network.
+	//
+	// Returned value:
+	//	WERROR_SUCCESS
+	//	WERROR_ERROR_INITFAILED
+	//	WERROR_ERROR_CONFFAILED
+	//	WERROR_ERROR_INTFAILURE
+	//
+	wError             err         = WERROR_SUCCESS;
+	wifi_init_config_t cfg         = WIFI_INIT_CONFIG_DEFAULT();
+	wifi_config_t      wifi_config = {0};
+	
+	strcpy((char*)wifi_config.sta.ssid,     ssidStr);
+	strcpy((char*)wifi_config.sta.password, pwdStr);
+	wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+	
+
+	// Init TCP/IP stack
+	if (esp_netif_init() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) TCP/IP stack initialization failed", __LINE__);
+		err = WERROR_ERROR_INITFAILED;
+		
+	// Default event loop creation
+	} else if (esp_event_loop_create_default() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) I cannot create the events handlers manager", __LINE__);
+		err = WERROR_ERROR_INITFAILED;
+	
+	// Network interface creating and connecting to the wi-fi driver
+	} else if (esp_netif_create_default_wifi_sta() == NULL) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) Network interface creation failed", __LINE__);
+		err = WERROR_ERROR_INITFAILED;
+
+	// It initializes WiFi subsystem and allocates resource for WiFi driver
+	} else if (esp_wifi_init(&cfg) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) esp_wifi_init() failed", __LINE__);
+		err = WERROR_ERROR_INITFAILED;
+
+	// Event handlers registering...
+	} else if (
+		esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID,    evh, NULL, NULL)  != ESP_OK ||
+		esp_event_handler_instance_register(IP_EVENT,   IP_EVENT_STA_GOT_IP, evh, NULL, NULL) != ESP_OK
+	) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) I cannot register the event handlers", __LINE__);
+		err = WERROR_ERROR_CONFFAILED;
+
+	// WiFi operating mode setting...
+	} else if (esp_wifi_set_mode(WIFI_MODE_STA) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi operating mode selecting failed", __LINE__);
+		err = WERROR_ERROR_CONFFAILED;
+
+	// WiFi connection configuring
+	} else if (esp_wifi_set_config(WIFI_IF_STA, &wifi_config) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi link configuration failed", __LINE__);
+		err = WERROR_ERROR_CONFFAILED;
+		
+	// Network connection start
+	} else if (esp_wifi_start() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi link starting failed", __LINE__);
+		err = WERROR_ERROR_INTFAILURE;
+	}
+
+	return(err);
+}
+
