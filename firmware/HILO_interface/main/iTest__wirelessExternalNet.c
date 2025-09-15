5ccd43b13598f567b6b494c143d5672a0e1ef14b
Silvano Catinella <silvano.catinella@fimer.com>
2025-09-15 16:21:55 +0200

    Test implemented ;-)

--- firmware/HILO_interface/main/iTest__wirelessExternalNet.c
+++ firmware/HILO_interface/main/iTest__wirelessExternalNet.c
@@ -38,18 +38,111 @@
 //
 //                                                                                                               cols=128 tab=6
 ------------------------------------------------------------------------------------------------------------------------------*/
-#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "wError.h"
+
 
 #ifndef HILO_SSIDNAME
-#define HILO_SSIDNAME ""
+#error "You have to define HILO_SSIDNAME symbol in the configuration file (read CMakeLists.txt file for further details)"
 #endif
 
 #ifndef HILO_PASSWORD
-#define HILO_PASSWORD ""
+#error "You have to define HILO_PASSWORD symbol in the configuration file (read CMakeLists.txt file for further details)"
 #endif
 
-void main() {
 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_CONNECTED) {
+		ESP_LOGI(__FILE__, "The ESP32 has successfully connected to the \"%s\" net", HILO_SSIDNAME);
+
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		ESP_LOGI(__FILE__, "Reconnecting in progress...");
+		esp_wifi_connect();
+
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(__FILE__, "IP address acknowledged: " IPSTR, IP2STR(&event->ip_info.ip));
+	}
 
 	return;
 }
+
+
+void wifi_init_sta (const char *ssidStr, const char *pwdStr) {
+	wifi_init_config_t cfg         = WIFI_INIT_CONFIG_DEFAULT();
+	wifi_config_t      wifi_config = {0};
+	
+	strcpy((char*)wifi_config.sta.ssid,     ssidStr);
+	strcpy((char*)wifi_config.sta.password, pwdStr);
+	wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+	
+
+	// Init TCP/IP stack
+	if (esp_netif_init() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) TCP/IP stack initialization failed", __LINE__);
+
+	// Default event loop creation
+	} else if (esp_event_loop_create_default() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) I cannot create the events handlers manager", __LINE__);
+	
+	// Network interface creating and connecting to the wi-fi driver
+	} else if (esp_netif_create_default_wifi_sta() == NULL) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) Network interface creation failed", __LINE__);
+
+	// It initializes WiFi subsystem and allocates resource for WiFi driver
+	} else if (esp_wifi_init(&cfg) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) ", __LINE__);
+
+	// Event handlers registering...
+	} else if (
+		esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL)  != ESP_OK ||
+		esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL) != ESP_OK
+	) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) I cannot register the event handlers", __LINE__);
+
+	// WiFi operating mode setting...
+	} else if (esp_wifi_set_mode(WIFI_MODE_STA) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi operating mode selecting failed", __LINE__);
+
+	// WiFi connection configuring
+	} else if (esp_wifi_set_config(WIFI_IF_STA, &wifi_config) != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi link configuration failed", __LINE__);
+
+	// Network connection start
+	} else if (esp_wifi_start() != ESP_OK) {
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) WiFi link starting failed", __LINE__);
+
+	} else 
+		ESP_LOGI(__FILE__, "*** wifi_init_sta finished. SSID:%s password:%s ***", HILO_SSIDNAME, HILO_PASSWORD);
+}
+
+
+void app_main(void) {
+	// Init NVS
+	if (nvs_flash_init() != ESP_OK)
+		// ERROR!
+		ESP_LOGE(__FILE__, "(%d) I cannot allocate NVS memory for the driver", __LINE__);
+	else
+		wifi_init_sta(HILO_SSIDNAME, HILO_PASSWORD);
+
+	return;
+}
+
