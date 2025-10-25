9881eb4747572752413c0540c443e88cfa5b4978
Silvano Catinella <silvano.catinella@fimer.com>
2025-10-25 16:55:42 +0200

    Out-to-date file-names fixed

---
+++ firmware/HILO_interface/components/testDataCompiler/include/testData_fixedTimePeriod.h
@@ -0,0 +1,45 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//
+//
+// File:  testData_fixedTimePeriod.h
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This module convert the test-data definition in a stream of bits.
+//		In this definition type, user can set a fixed period where a defined bit must have a constant value.
+//		This value can be set as "swap", in this case the new value will be the inverted old one 
+//		
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef __TD_FIXEDTIMEPERIOD__
+#define __TD_FIXEDTIMEPERIOD__
+
+#include <wError.h>
+
+wError fixedTimePeriod_init();
+wError fixedTimePeriod_check();
+wError fixedTimePeriod_generate();
+
+#endif
---
+++ firmware/HILO_interface/components/testDataCompiler/include/testData_squareWave.h
@@ -0,0 +1,43 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//
+//
+// File:  testData_squareWave.h
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This module convert the test-data definition in a stream of bits.
+//		In this definition type, user can set a digital wave as test-data
+//		
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+#ifndef __TD_SQUAREWAVE__
+#define __TD_SQUAREWAVE__
+
+#include <wError.h>
+
+wError squareWave_init();
+wError squareWave_check();
+wError squareWave_generate();
+
+#endif
