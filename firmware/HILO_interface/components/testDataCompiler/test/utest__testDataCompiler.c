775c48539f199e02b5e2832b967c1b29638d4253
scatinella <catinella@yahoo.com>
2025-11-01 12:13:43 +0100

    More accurate tests for sub-modules' functions

--- firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
+++ firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
@@ -1,6 +1,28 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//
+//
+// File:   utest__testDataCompiler.c
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	Unit-tests for the testDataCompiler fabric and its sub-modules
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+
 #include <testDataCompiler.h>
 #include <debugTools.h>
+#include <testData_fixedTimePeriod.h>
+#include <testData_number.h>
+#include <testData_squareWave.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 char *FTP_jsonMsg = "{                \
  	\"type\":  \"fixedTimePeriod\", \
@@ -21,6 +43,31 @@ char *SQW_jsonMsg = "{              \
 	\"dutyCycle\": 50             \
 }";
 
+char *jsonMsg = "{                                   \
+	configuration: {                               \
+		freq: 5                                  \
+	}                                              \
+	test_outputData: [                             \
+		{                                        \
+			\"type\":  \"fixedTimePeriod\",    \
+			\"pin\":   5,                      \
+			\"value\": \"high\"                \
+		}, {                                     \
+			\"type\":  \"number\",             \
+			\"pins\":  [8,9,10,11,12,13,14,15],\
+			\"period\": 10                     \
+		}, {                                     \
+			\"type\":      \"squareWave\",     \
+			\"pin\":       5,                  \
+			\"period\":    10,                 \
+			\"dutyCycle\": 50                  \
+		}                                        \
+	]                                              \
+}";
+
+//------------------------------------------------------------------------------------------------------------------------------
+//                                              U N I T   T E S T S
+//------------------------------------------------------------------------------------------------------------------------------
 
 TEST (T1, testDataCompiler_init) {
 	wError err = WERROR_SUCCESS;
@@ -32,40 +79,155 @@ TEST (T1, testDataCompiler_init) {
 	return;
 }
 
-
-TEST (T2, testDataCompiler_generate) {
+TEST (T2, testData_squareWave_check) {
 	wError err = WERROR_SUCCESS;
-	cJSON  *j_fixedTimePeriod = NULL;
-	cJSON  *j_number          = NULL;
-	cJSON  *j_squareWave      = NULL;
-	
-	// JSON messages creation...
-	j_fixedTimePeriod = cJSON_Parse(FTP_jsonMsg);
-	j_number          = cJSON_Parse(NUM_jsonMsg);
-	j_squareWave      = cJSON_Parse(SQW_jsonMsg);
+	cJSON  *j_squareWave = NULL;
 
-	if (j_fixedTimePeriod == NULL || j_number == NULL || j_squareWave == NULL) {
+	
+	if ((j_squareWave = cJSON_Parse(NUM_jsonMsg)) == NULL) {
 		// ERROR!
+		err = WERROR_ERRUTEST_CORRUPTDATA;
+		ERRORBANNER (err);
 		fprintf(stderr, "ERROR! JSON message parsing failed\n");
 
 	} else {
-		err = testDataCompiler_generate(j_fixedTimePeriod);
-		ASSERT_EQ (WERROR_SUCCESS, err);
+		//
+		// Test target: wrong JSON data
+		// 	For every JSON that does not match with the expected type, the <sub.module>_check() function returns a warning
+		// 	code
+		//
+		err = testData_squareWave_check(j_squareWave);
+		ASSERT_TRUE (WERROR_ISWARNING(err));
 
-		err = testDataCompiler_generate(j_number);
-		ASSERT_EQ (WERROR_SUCCESS, err);
+		free(j_squareWave);
+		j_squareWave = NULL;
+		
+		if ((j_squareWave = cJSON_Parse(SQW_jsonMsg)) == NULL) {
+			// ERROR!
+			err = WERROR_ERRUTEST_CORRUPTDATA;
+			ERRORBANNER (err);
+			fprintf(stderr, "ERROR! JSON message parsing failed\n");
 
-		err = testDataCompiler_generate(j_squareWave);
-		ASSERT_EQ (WERROR_SUCCESS, err);
+		} else {
+			//
+			// Test target: right JSON data
+			//
+			err = testData_squareWave_check(j_squareWave);
+			ASSERT_TRUE (WERROR_ISSUCCESS(err));
+		}
 	}
 
 	return;
 }
 
 
+TEST (T3, testData_number_check) {
+	wError err = WERROR_SUCCESS;
+	cJSON  *j_number = NULL;
+	
+	if ((j_number = cJSON_Parse(SQW_jsonMsg)) == NULL) {
+		// ERROR!
+		err = WERROR_ERRUTEST_CORRUPTDATA;
+		ERRORBANNER (err);
+		fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+	} else {
+		//
+		// Test target: wrong JSON data
+		// 	For every JSON that does not match with the expected type, the <sub.module>_check() function returns a warning
+		// 	code
+		//
+		err = testData_number_check(j_number);
+		ASSERT_TRUE (WERROR_ISWARNING(err));
+
+		free(j_number);
+		j_number = NULL;
+		
+		if ((j_number = cJSON_Parse(NUM_jsonMsg)) == NULL) {
+			// ERROR!
+			err = WERROR_ERRUTEST_CORRUPTDATA;
+			ERRORBANNER (err);
+			fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+		} else {
+			//
+			// Test target: right JSON data
+			//
+			err = testData_number_check(j_number);
+			ASSERT_TRUE (WERROR_ISSUCCESS(err));
+		}
+	}
+
+	return;
+}
+
+
+TEST (T4, testData_fixedTimePeriod_check) {
+	wError err = WERROR_SUCCESS;
+	cJSON  *j_fixedTimePeriod = NULL;
+	
+	if ((j_fixedTimePeriod = cJSON_Parse(SQW_jsonMsg)) == NULL) {
+		// ERROR!
+		err = WERROR_ERRUTEST_CORRUPTDATA;
+		ERRORBANNER (err);
+		fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+	} else {
+		//
+		// Test target: wrong JSON data
+		// 	For every JSON that does not match with the expected type, the <sub.module>_check() function returns a warning
+		// 	code
+		//
+		err = testData_fixedTimePeriod_check(j_fixedTimePeriod);
+		ASSERT_TRUE (WERROR_ISWARNING(err));
+
+		free(j_fixedTimePeriod);
+		j_fixedTimePeriod = NULL;
+		
+		if ((j_fixedTimePeriod = cJSON_Parse(FTP_jsonMsg)) == NULL) {
+			// ERROR!
+			err = WERROR_ERRUTEST_CORRUPTDATA;
+			ERRORBANNER (err);
+			fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+		} else {
+			//
+			// Test target: right JSON data
+			//
+			err = testData_fixedTimePeriod_check(j_fixedTimePeriod);
+			ASSERT_TRUE (WERROR_ISSUCCESS(err));
+		}
+	}
+
+	return;
+}
+
+TEST (T5, testDataCompiler_generate) {
+/*
+	wError err = WERROR_SUCCESS;
+	cJSON  *jmsg = NULL;
+	
+	// JSON messages creation...
+	if ((jmsg = cJSON_Parse(FTP_jsonMsg)) == NULL) {
+		// ERROR!
+		err = WERROR_ERRUTEST_CORRUPTDATA;
+		ERRORBANNER (err);
+		fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+	} else {
+		err = testDataCompiler_generate(jmsg);
+		ASSERT_TRUE (WERROR_ISSUCCESS(err));
+	}
+*/
+	return;
+}
+
+
 int main() {
 	T1__testDataCompiler_init();
-	T2__testDataCompiler_generate();
-
+	T2__testData_squareWave_check();
+	T3__testData_number_check();
+	T4__testData_fixedTimePeriod_check();
+	T5__testDataCompiler_generate();
 	return(0);
 }
