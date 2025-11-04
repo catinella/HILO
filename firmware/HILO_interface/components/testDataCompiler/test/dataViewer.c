ead31970d8c5f4ef39dd4036d8ce64f08adbbc31
scatinella <catinella@yahoo.com>
2025-11-04 20:19:24 +0100

    Standard documentation added

--- firmware/HILO_interface/components/testDataCompiler/test/dataViewer.c
+++ firmware/HILO_interface/components/testDataCompiler/test/dataViewer.c
@@ -1,4 +1,38 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//
+//
+// File:   dataViewer.c
+//
+// Author: Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		It is a useful tool to check for the resulted data by the mocked functions. This tool realizes a virtual digital
+//		states alalizer, mainly. Its TUI uses NCURSES library, so check for this dependence before to compile it.
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+------------------------------------------------------------------------------------------------------------------------------*/
 #include <stdint.h>
+#include <debugTools.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -62,6 +96,9 @@ wError dataDrawing (WINDOW *win, FILE *fh, uint32_t offset) {
 	return (err);
 }
 
+//------------------------------------------------------------------------------------------------------------------------------
+//                                                      M A I N
+//------------------------------------------------------------------------------------------------------------------------------
 int main (int argc, char *argv[]) {
 	wError err = WERROR_SUCCESS;
 	FILE   *fh = NULL;
