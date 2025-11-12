7acedf543e136bda058bcec38f2dc7a282872beb
Silvano Catinella <silvano.catinella@fimer.com>
2025-11-12 21:46:24 +0100

    Enhancement for TEST(T5, testDataCompiler_generate)

--- firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
+++ firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
@@ -52,6 +52,21 @@ char *SQW_jsonMsg = "{               \
 	\"stop\":      40              \
 }";
 
+char *jsonMsgFxp1 = "{                                   \
+	\"configuration\": {                               \
+		\"freq\": 5                                  \
+	},                                                 \
+	\"test_outputData\": [                             \
+		{                                            \
+			\"type\":      \"fixedTimePeriod\",    \
+			\"pin\":       5,                      \
+			\"value\":     \"high\",               \
+			\"start\":     10,                     \
+			\"stop\":      15                      \
+		}                                            \
+	]                                                  \
+}";
+
 char *jsonMsg = "{                                       \
 	\"configuration\": {                               \
 		\"freq\": 5                                  \
@@ -328,9 +343,13 @@ TEST (T4, testData_fixedTimePeriod_check) {
 TEST (T5, testDataCompiler_generate) {
 	wError err = WERROR_SUCCESS;
 	cJSON  *jmsg = NULL;
+
+	testDataCompiler_clean();
 	
-	// JSON messages creation...
-	if ((jmsg = cJSON_Parse(jsonMsg)) == NULL) {
+	//
+	// Simple test for fixedTimePeriod() module
+	//
+	if ((jmsg = cJSON_Parse(jsonMsgFxp1)) == NULL) {
 		// ERROR!
 		err = WERROR_ERRUTEST_CORRUPTDATA;
 		ERRORBANNER (err);
@@ -339,6 +358,68 @@ TEST (T5, testDataCompiler_generate) {
 	} else {
 		err = testDataCompiler_generate(jmsg);
 		ASSERT_TRUE ((WERROR_ISERROR(err) == false));
+
+		if (WERROR_ISERROR(err) == false) {
+			//
+			// Checking for the virtual-SRAM content
+			//	Because the sampling rate is 5Khz, the period is 0.2ms.
+			//	In the test definition the bit number 5 will be HIGH only after 10ms. It means after (10/0.2) 50 samples.
+			//	The signal must return LOW after 15ms from the test beginning. It means it must stay HIGH for 25 samples.
+			//
+
+			FILE     *fh    = NULL;
+			uint16_t buffer = 0;
+			
+			if ((fh = fopen(UTFILE, "r")) == NULL) {
+				// ERROR!
+				err = WERROR_ERRUTEST_IOERROR;
+				ERRORBANNER (err);
+				fprintf(stderr, "ERROR! I cannot open the virtual-SRAM (%s)\n", UTFILE);
+
+			} else if (fseek(fh, 0, SEEK_SET) < 0) {
+				// ERROR!
+				err = WERROR_ERRUTEST_IOERROR;
+				ERRORBANNER (err);
+				fprintf(stderr, "ERROR! I cannot index the virtual-SRAM\n");
+
+			} else {
+				uint8_t st = 0;
+				uint16_t t = 0;
+				
+				while (WERROR_ISERROR(err) == false) {
+					if      (t == 50)   st = 1;
+					else if (t == 75)   st = 1;
+					else if (t == 1024) break;
+					
+					if (fread(&buffer, sizeof(buffer), 1, fh) != 1) {
+						// ERROR!
+						err = WERROR_ERRUTEST_IOERROR;
+						ERRORBANNER (err);
+						fprintf(stderr, "ERROR! I cannot read the virtual-SRAM\n");
+						break;
+	
+					} else if (buffer != 0 && st == 0) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+						break;
+
+					} else if (buffer != 0x20 && st == 1) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+						break;
+					
+					} else if (buffer != 0 && st == 2) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+						break;
+					}
+
+					t++;
+				}
+				
+				ASSERT_TRUE ((WERROR_ISERROR(err) == false));
+			}
+		}
 	}
 
 	return;
