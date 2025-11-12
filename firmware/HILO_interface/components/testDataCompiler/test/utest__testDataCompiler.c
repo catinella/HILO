32e046ca7841ea8ece37e41f2faff0547f94891e
Silvano Catinella <silvano.catinella@fimer.com>
2025-11-12 22:09:51 +0100

    T3::testDataCompiler_generate() added

--- firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
+++ firmware/HILO_interface/components/testDataCompiler/test/utest__testDataCompiler.c
@@ -53,6 +53,21 @@ char *jsonMsgFxp2 = "{                                   \
 	]                                                  \
 }";
 
+char *jsonMsgFxp3 = "{                                   \
+	\"configuration\": {                               \
+		\"freq\": 5                                  \
+	},                                                 \
+	\"test_outputData\": [                             \
+		{                                            \
+			\"type\":      \"fixedTimePeriod\",    \
+			\"pin\":       5,                      \
+			\"value\":     \"swap\",               \
+			\"start\":     10,                     \
+			\"stop\":      16                      \
+		}                                            \
+	]                                                  \
+}";
+
 char *jsonMsg = "{                                       \
 	\"configuration\": {                               \
 		\"freq\": 5                                  \
@@ -206,7 +221,7 @@ TEST (T2, testDataCompiler_generate) {
 			// Checking for the virtual-SRAM content
 			//	Because the sampling rate is 5Khz, the period is 0.2ms.
 			//	In the test definition the bit number 5 will be HIGH for 30ms by the beginning.
-			//	It means after (30/0.2) 60 samples.
+			//	It means after (30/0.2) 150 samples.
 			//	The signal must return LOW after that time
 			//
 
@@ -230,7 +245,7 @@ TEST (T2, testDataCompiler_generate) {
 				uint16_t t = 0;
 				
 				while (WERROR_ISERROR(err) == false) {
-					if      (t == 60)   st = 1;
+					if      (t == 150)   st = 1;
 					else if (t == 1024) break;
 					
 					if (fread(&buffer, sizeof(buffer), 1, fh) != 1) {
@@ -238,17 +253,14 @@ TEST (T2, testDataCompiler_generate) {
 						err = WERROR_ERRUTEST_IOERROR;
 						ERRORBANNER (err);
 						fprintf(stderr, "ERROR! I cannot read the virtual-SRAM\n");
-						break;
 	
 					} else if (buffer != 0x20 && st == 0) {
 						// TEST FAILED
 						err = WERROR_ERRUTEST_TESTFAILED;
-						break;
 
 					} else if (buffer != 0 && st == 1) {
 						// TEST FAILED
 						err = WERROR_ERRUTEST_TESTFAILED;
-						break;
 					}
 
 					t++;
@@ -262,6 +274,93 @@ TEST (T2, testDataCompiler_generate) {
 	return;
 }
 
+
+TEST (T3, testDataCompiler_generate) {
+	//
+	// [!] This test requires to run after the T2::testDataCompiler_generate test. Because the current one uses data
+	//     written by the previouse one.
+	//
+	wError err = WERROR_SUCCESS;
+	cJSON  *jmsg = NULL;
+	
+	//
+	// Simple test for fixedTimePeriod() module
+	//
+	if ((jmsg = cJSON_Parse(jsonMsgFxp3)) == NULL) {
+		// ERROR!
+		err = WERROR_ERRUTEST_CORRUPTDATA;
+		ERRORBANNER (err);
+		fprintf(stderr, "ERROR! JSON message parsing failed\n");
+
+	} else {
+		err = testDataCompiler_generate(jmsg);
+		ASSERT_TRUE ((WERROR_ISERROR(err) == false));
+
+		if (WERROR_ISERROR(err) == false) {
+			//
+			// Checking for the virtual-SRAM content
+			//	Because the sampling rate is 5Khz, the period is 0.2ms.
+			//	In the test definition the bit number 5 will be swapped for 6ms after 10ms by the test beginning.
+			//
+
+			FILE     *fh    = NULL;
+			uint16_t buffer = 0;
+			
+			if ((fh = fopen(UTFILE, "r")) == NULL) {
+				// ERROR!
+				err = WERROR_ERRUTEST_IOERROR;
+				ERRORBANNER (err);
+				fprintf(stderr, "ERROR! I cannot open the virtual-SRAM (%s)\n", UTFILE);
+
+			} else if (fseek(fh, 0, SEEK_SET) < 0) {
+				// ERROR!
+				err = WERROR_ERRUTEST_IOERROR;
+				ERRORBANNER (err);
+				fprintf(stderr, "ERROR! I cannot index the virtual-SRAM\n");
+
+			} else {
+				uint8_t st = 0;
+				uint16_t t = 0;
+				
+				while (WERROR_ISERROR(err) == false) {
+					if      (t == 50)   st = 1;
+					else if (t == 80)   st = 2;
+					else if (t == 150)  st = 3;
+					else if (t == 1024) break;
+					
+					if (fread(&buffer, sizeof(buffer), 1, fh) != 1) {
+						// ERROR!
+						err = WERROR_ERRUTEST_IOERROR;
+						ERRORBANNER (err);
+						fprintf(stderr, "ERROR! I cannot read the virtual-SRAM\n");
+	
+					} else if (buffer != 0x20 && st == 0) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+
+					} else if (buffer != 0 && st == 1) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+
+					} else if (buffer != 0x20 && st == 2) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+
+					} else if (buffer != 0 && st == 3) {
+						// TEST FAILED
+						err = WERROR_ERRUTEST_TESTFAILED;
+					}
+
+					t++;
+				}
+				
+				ASSERT_TRUE ((WERROR_ISERROR(err) == false));
+			}
+		}
+	}
+
+	return;
+}
 TEST (T1, testDataCompiler_getParams) {
 	wError     err = WERROR_SUCCESS;
 	configDB_t cdata; 
@@ -276,6 +375,7 @@ int main() {
 	T1__testDataCompiler_init();
 	T1__testDataCompiler_generate();
 	T2__testDataCompiler_generate();
+	T3__testDataCompiler_generate();
 	T1__testDataCompiler_getParams();
 	
 	return(0);
