5515101fb0ae8842b453f821cfae16aa4c3da04b
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-08 18:59:49 +0100

    Preliminary interactive test code added

---
+++ console/PinStrip/test/test.cpp
@@ -0,0 +1,37 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+//
+//
+// Filename: test.cpp
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//		This folder hosts the test of the PinStrip widget.
+//		The test will display a fake device with 16 pins. You can connect and disconnect every item belongs to the PinStrip to one
+//		those 16 pins
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
