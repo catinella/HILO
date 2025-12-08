245e227d3bda17b6ce0017121549dd786dac1499
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-08 09:41:42 +0100

    New widget used by all HILO's tools
    This widget is used by all the others to connect themselves to the HILO's input/output pins

---
+++ console/PinStrip/PinStrip.cpp
@@ -0,0 +1,51 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+//
+//
+// Filename: PinStrip.cpp
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//
+//
+//
+// License:  LGPL ver 3.0
+//
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
+#include "PinStrip.h"
+#include "ui_PinStrip.h"
+
+PinStrip::PinStrip(QWidget *parent) : QWidget(parent), ui(new Ui::PinStrip) {
+	ui->setupUi(this);
+
+	// Qui colleghi i segnali dei pin, imposti stati iniziali, ecc.
+	// esempio:
+	// connect(ui->chkPin1, &QCheckBox::toggled, this, &PinStrip::onPin1Toggled);
+}
+
+PinStrip::~PinStrip() {
+	delete ui;
+}
+
---
+++ console/PinStrip/PinStrip.pro
@@ -0,0 +1,60 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+#                                                    _   _ ___ _     ___  
+#                                                   | | | |_ _| |   / _ \
+#                                                   | |_| || || |  | | | |
+#                                                   |  _  || || |__| |_| |
+#                                                   |_| |_|___|_____\___/ 
+#                                                    Hardware in the loop
+#                                              (https://github.com/catinella/HILO)
+#
+#
+#
+#
+# Filename: PinStrip.pro
+#
+# Author:   Silvano Catinella <catinella@yahoo.com>
+#
+# Description:
+#	QMake setting to creates the Makefile. In this file you can find all rules to build the source dode
+# 
+#	TEMPLATE = {app|lib|subdirs|aux}
+#		app     Executable file
+#		lib     Library (lib.a, lib.so) file
+#		subdirs It creates a Makefile in every sub-folder where there is a file.pro
+#		aux     Script driven building process
+#
+#-------------------------------------------------------------------------------------------------------------------------------
+
+CONFIG_FILE = "$$PWD/../conf.pri"
+TEMPLATE    = lib
+SOURCES    += $$files($$PWD/*.cpp)
+HEADERS    += $$files($$PWD/*.h)
+FORMS      += $$PWD/PinStrip.ui
+CONFIG     += staticlib
+TARGET      = PinStrip
+QT         += widgets
+
+exists($$CONFIG_FILE) {
+	message("[i] configuration file $$CONFIG_FILE detected")
+	include($$CONFIG_FILE)
+} else {
+	# Checking for environment variables
+	GDB = $$(GDB)
+}
+
+equals(GDB, 1) {
+	message("WARNING! You are building $$TARGET module in debug mode")
+	CONFIG  += debug
+	CONFIG  -= release
+} else {
+	message("[i] You are building $$TARGET in release mode")
+	CONFIG  += release
+	CONFIG  -= debug
+}
+
+cleanall.target   = cleanall
+cleanall.commands = $$escape_expand(@rm -fv lib$${TARGET}.a Makefile)
+cleanall.depends  = clean
+
+QMAKE_EXTRA_TARGETS += cleanall
+cleanall
