5a5748d68338293e9e212efad3da832b017d134b
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-08 09:45:06 +0100

    standard header comment added

--- console/KeypadWidget/KeypadWidget.h
+++ console/KeypadWidget/KeypadWidget.h
@@ -1,13 +1,39 @@
-//------------------------------------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
 //
 //
 //
 //
+// Filename: KeypadWidget.h
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
 //
 //
 //
+// License:  LGPL ver 3.0
 //
-//------------------------------------------------------------------------------------------------------------------------------
+// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file  "Linuxwoodo/trunk/templates/lgpl-3.0.txt".
+// 		You should have received a copy of the GNU General Public License along with this file; 
+// 		if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//                                                                                                               cols=128 tab=6
+------------------------------------------------------------------------------------------------------------------------------*/
 
 #ifndef KEYPADWIDGET_H
 #define KEYPADWIDGET_H
