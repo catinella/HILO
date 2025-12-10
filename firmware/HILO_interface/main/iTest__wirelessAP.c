572066f5882eea78f34ecd1f8146453bfe2892f8
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-11 00:00:03 +0100

    Stupid typing-error in a comment
    Copy and paste of the error in many files :-(

--- firmware/HILO_interface/main/iTest__wirelessAP.c
+++ firmware/HILO_interface/main/iTest__wirelessAP.c
@@ -18,7 +18,7 @@
 //
 // License:  LGPL ver 3.0
 //
-// 		This script is a wfree software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		This script is a free software; you can redistribute it and/or modify it under the terms	of the GNU
 // 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
 // 		or (at your option) any later version. 
 //
