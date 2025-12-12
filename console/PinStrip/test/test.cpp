bd1d58607d4921929088fcdd4605ce2a0412e5fb
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-12 22:40:21 +0100

    TestWindow class definition has been moved.
    Because QT-MOC process detect the header files to create mock-files, TestWindow class has been moved to its own file TestWindow.{cpp|h}

--- console/PinStrip/test/test.cpp
+++ console/PinStrip/test/test.cpp
@@ -16,8 +16,6 @@
 //
 // Description:
 //		This folder hosts the test of the PinStrip widget.
-//		The test will display a fake device with 16 pins. You can connect and disconnect every item belongs to the PinStrip to one
-//		those 16 pins
 //
 // License:  LGPL ver 3.0
 //
@@ -37,105 +35,15 @@
 ------------------------------------------------------------------------------------------------------------------------------*/
 
 #include <QApplication>
-#include <QMainWindow>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QPushButton>
-#include <QPainter>
-#include <QCheckBox>
-#include <QVector>
-#include "PinStrip.h"
+#include "TestWidget.h"
 
-#define TEST_NUMOFPINS 8
 
-class TestWindow:public QMainWindow {
-public:
-	explicit TestWindow (QWidget * parent = nullptr): QMainWindow (parent) {
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
 
-		auto *central       = new QWidget(this);
-		auto *layout        = new QVBoxLayout(central);
-		auto *buttonsLayout = new QHBoxLayout;
+	TestWidget w;
+	w.show();
 
-		strip = new PinStrip(central);
-		
-		layout->addLayout(buttonsLayout);
-		layout->addWidget(strip);
-		layout->setContentsMargins(10, 10, 10, 10);
-		setCentralWidget(central);
-		setWindowTitle("PinStrip test window");
-		resize(300, 500);
-
-		// Buttons creation...
-		for (int i = 0; i < TEST_NUMOFPINS; ++i) {
-			auto *btn = new QPushButton(QString::number(i), this);
-			btn->setCheckable(true);
-			buttonsLayout->addWidget(btn);
-			m_buttons.push_back(btn);
-		}
-
-		// Looking for the center points used for the graphical linking
-		{
-			for (int i = 0; i < TEST_NUMOFPINS; ++i) {
-				m_pins.push_back(
-					strip->findChild<QCheckBox*>(QString("pin%1").arg(i))
-				);
-			}
-		}
-
-		// Connections drawing...
-		for (int i = 0; i < TEST_NUMOFPINS; ++i) {
-			if (m_buttons[i] && m_pins[i]) {
-				connect(m_buttons[i], &QPushButton::toggled, this, [this, i](bool checked) {
-					if (m_pins[i]) m_pins[i]->setChecked(checked);
-					update();   // ridisegna le linee (non strettamente necessario, ma esplicito)
-				});
-			}
-		}
-
-		layout->setContentsMargins(10, 10, 10, 10);
-		layout->setSpacing(10);
-	}
-
-protected:
-	void paintEvent(QPaintEvent *event) override {
-		QMainWindow::paintEvent(event);
-
-		QPainter p(this);
-		p.setRenderHint(QPainter::Antialiasing, true);
-
-		QPen pen;
-		pen.setWidth(1);
-		p.setPen(pen);
-
-		int n = std::min(m_buttons.size(), m_pins.size());
-
-		for (int i = 0; i < n; ++i) {
-			if (!m_buttons[i] || !m_pins[i])
-			continue;
-
-			// centro del pulsante in coordinate della finestra
-			QPoint bCenter = m_buttons[i]->mapTo(this,
-			m_buttons[i]->rect().center());
-
-			// centro del checkbox in coordinate della finestra
-			QPoint pinCenter = m_pins[i]->mapTo(this,
-			m_pins[i]->rect().center());
-
-			p.drawLine(bCenter, pinCenter);
-		}
-	}
-
-private:
-	PinStrip *strip = nullptr;
-	QVector<QPushButton*> m_buttons;
-	QVector<QCheckBox*>   m_pins;
-};
-
-int main (int argc, char *argv[]) {
-	QApplication app (argc, argv);
-
-	TestWindow w;
-	w.show ();
-
-	return app.exec ();
+	return app.exec();
 }
+
