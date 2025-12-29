f250ca9a14527b7b339b77cf89dea6162b10fb04
Silvano Catinella <silvano.catinella@fimer.com>
2025-12-29 10:16:19 +0100

    PinStrip widget added (uncompleted version)

--- console/inputDevs/KeypadWidget/KeypadWidget.cpp
+++ console/inputDevs/KeypadWidget/KeypadWidget.cpp
@@ -40,8 +40,22 @@
 #include <QPushButton>
 #include <QDebug>
 
-KeypadWidget::KeypadWidget (QWidget *parent) : QWidget(parent), ui(new Ui::KeypadWidget) {
-	ui->setupUi (this);
+void KeypadWidget::appendDigit (const QString & d) {
+	auto txt = ui->displayEdit->text();	// <--- al posto di m_display
+	txt.append(d);
+	ui->displayEdit->setText(txt);
+	emit textChanged(txt);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+//                                          P U B L I C   M E T H O D S
+//------------------------------------------------------------------------------------------------------------------------------
+KeypadWidget::KeypadWidget (QWidget *parent):
+	QWidget(parent),
+	ui(new Ui::KeypadWidget),
+	myPins(PWDG_TOOLSIDE, KEYPAD_DATABITS)
+{
+	ui->setupUi(this);
 	digitButtons = {
 		ui->pushButton_0,
 		ui->pushButton_1,
@@ -59,54 +73,50 @@ KeypadWidget::KeypadWidget (QWidget *parent) : QWidget(parent), ui(new Ui::Keypa
 	// Numeric buttons
 	//
 	for (auto btn : digitButtons)
-		connect (btn, &QPushButton::clicked, this, &KeypadWidget::onDigitClicked);
-	
+		connect(btn, &QPushButton::clicked, this, &KeypadWidget::onDigitClicked);
 	
 	//
 	// Special buttons
 	//
-	connect (ui->pushButton_canc, &QPushButton::clicked, this, &KeypadWidget::onClearClicked);
-	connect (ui->pushButton_del,  &QPushButton::clicked, this, &KeypadWidget::onBackspaceClicked);
-	connect (ui->pushButton_ack,  &QPushButton::clicked, this, &KeypadWidget::onOkClicked);
+	connect(ui->pushButton_canc, &QPushButton::clicked, this, &KeypadWidget::onClearClicked);
+	connect(ui->pushButton_del,  &QPushButton::clicked, this, &KeypadWidget::onBackspaceClicked);
+	connect(ui->pushButton_ack,  &QPushButton::clicked, this, &KeypadWidget::onOkClicked);
 }
 
 KeypadWidget::~KeypadWidget () {
 	delete ui;
 }
 
-void KeypadWidget::appendDigit (const QString & d) {
-	auto txt = ui->displayEdit->text ();	// <--- al posto di m_display
-	txt.append (d);
-	ui->displayEdit->setText (txt);
-	emit textChanged (txt);
-}
-
 void KeypadWidget::onDigitClicked () {
-	auto *btn = qobject_cast < QPushButton * >(sender ());
-	if (!btn)
-		return;
-	const QString d = btn->text ();
-	appendDigit (d);
-	bool ok = false;
-	int val = d.toInt (&ok);
-	if (ok)
-		emit digitPressed (val);
+	//
+	// Description:
+	//	This slot is connected to every numeric button. It add the typed digit to the number you see on the Keypad's display
+	//
+	auto *btn = qobject_cast<QPushButton*>(sender());
+	if (btn) {
+		const QString d = btn->text();
+		appendDigit(d);
+		bool ok = false;
+		int val = d.toInt(&ok);
+		if (ok)
+			emit digitPressed(val);
+	}
 }
 
 void KeypadWidget::onClearClicked () {
-	ui->displayEdit->clear ();
-	emit textChanged (QString ());
+	ui->displayEdit->clear();
+	emit textChanged(QString());
 }
 
 void KeypadWidget::onBackspaceClicked () {
 	auto txt = ui->displayEdit->text ();
-	if (!txt.isEmpty ()) {
-		txt.chop (1);
-		ui->displayEdit->setText (txt);
-		emit textChanged (txt);
+	if (!txt.isEmpty()) {
+		txt.chop(1);
+		ui->displayEdit->setText(txt);
+		emit textChanged(txt);
 	}
 }
 
-void KeypadWidget::onOkClicked () {
-	emit enterPressed (ui->displayEdit->text ());
+void KeypadWidget::onOkClicked() {
+	emit enterPressed(ui->displayEdit->text());
 }
