71ea64bb9500922ad244a8aeaa489dacebcb9968
scatinella <catinella@yahoo.com>
2026-09-22 19:23:47 +0200

    Empty test added

---
+++ firmware/HILO_engine/Core/test/test_ringBuffer.c
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------------------------------------------------------------
+//                                                    _   _ ___ _     ___  
+//                                                   | | | |_ _| |   / _ \
+//                                                   | |_| || || |  | | | |
+//                                                   |  _  || || |__| |_| |
+//                                                   |_| |_|___|_____\___/ 
+//                                                    Hardware in the loop
+//                                              (https://github.com/catinella/HILO)
+//
+//
+// Filename: test_ringBuffer.c
+//
+// Author:   Silvano Catinella <catinella@yahoo.com>
+//
+// Description:
+//	
+//
+------------------------------------------------------------------------------------------------------------------------------*/
+#include <minute.h>
+
+TEST(ringBuffer, T1) {
+}
+
+TEST(ringBuffer, T2) {
+}
+
+TEST(ringBuffer, T3) {
+}
+
+#include <test_ringBuffer.sgc>
